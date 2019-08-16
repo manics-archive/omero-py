ae2acb248a4e770c7d5dd21f43943a364faea243
Simon Li <spli@dundee.ac.uk>
2019-08-16 13:48:32 +0100

    Override the version string for dev builds

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -2,6 +2,12 @@
 set -eux
 
 if [ "${PLATFORM}" == "miniconda" ]; then
+    # Override the version string for dev builds
+    if [ -z "${TRAVIS_TAG}" ]; then
+      export VERSION_SUFFIX=".${TRAVIS_BRANCH}";
+    else
+      export VERSION_SUFFIX="";
+    fi
     conda build -c ${REQUIREMENTS_CHANNEL} ./recipe
 else
     docker build -t omero-py .
